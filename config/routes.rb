664134bc7f84d86ea2665052b95e88b0c9dd16dd
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-14 10:02:20 -0600

    Add spec for show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "items", to: "items#index"
+      get "itmes/:id", to: "items#show"
     end
   end
 
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe Api::V1::ItemsController do
     refute items.first[:created_at]
     refute items.last[:updated_at]
   end
+
+  it "returns a single item" do
+    item = Item.create(name: "great things", description: "it is great")
+
+    get :show, format: :json
+    expect(response).to have_http_status(:success)
+    item = JSON.pasrse(response.body, symbolize_names: true)
+    expect(item[:name]).to eq "great things"
+  end
 end
