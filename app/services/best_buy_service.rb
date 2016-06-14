4c7df6c23b910417b1c7d2761e85ad4c5dd1c964
Chad Ellison <chad.ellison0123@gmail.com>
2016-06-14 12:03:42 -0600

    previous commit has entire test suite passing with functionality-- was trying to make zip dynamic-- i stashed not realizing i had already begun refacotring this

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -2,17 +2,16 @@ class BestBuyService
   attr_reader :connection
 
   def initialize
-    @connection = Faraday.new(url: "https://api.bestbuy.com/v1/stores")
+    @connection = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,city,distance,phone,location,name&pageSize=15&apiKey=445jt4mb4gsenju48n2ndu8d")
     @connection.params["apiKey"] = ENV["apiKey"]
   end
 
-  def stores_hash(zip)
-    parse(nearby_stores(zip))
+  def stores_hash
+    parse(nearby_stores
   end
 
-  def nearby_stores(zip)
-    binding.pry
-    connection.get "(area(#{zip},25))?format=json&show=storeId,storeType,city,distance,phone,location,name&pageSize=15"
+  def nearby_stores
+    connection.get
   end
 
   def parse(response)
